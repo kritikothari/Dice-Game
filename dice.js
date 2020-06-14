47b0130351ba02f67b621ecdd2ff4f08cd8560aa
Kriti Kothari <55087613+kritikothari@users.noreply.github.com>
2020-06-15 03:17:09 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,17 @@
+var randomNumber1 =Math.floor(Math.random()*6)+1;
+var imageSource1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",imageSource1);
+var randomNumber2 =Math.floor(Math.random()*6)+1;
+var imageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",imageSource2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelectorAll("h1")[0].innerHTML = "🚩Player 1 wins";
+}else if(randomNumber1<randomNumber2){
+  document.querySelectorAll("h1")[0].innerHTML = "Player 2 wins🚩";
+}else {
+  document.querySelectorAll("h1")[0].innerHTML = "😎 Draw! 😎";
+}
